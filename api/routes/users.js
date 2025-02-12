04d8205001a634c6a32cbe08472b9708fe0c4c89
kmarcinkowskij <km.dartmoor3@gmail.com>
2025-02-12 23:13:35 +0100

    added a new route to the users router ("/get_plans")

--- api/routes/users.js
+++ api/routes/users.js
@@ -11,6 +11,7 @@ import {
 } from "../commons/common_functions.js";
 import {Types} from "mongoose";
 import plan from "../schemas/plan.js";
+import Plan from "../schemas/plan.js";
 
 const users_router = express.Router();
 
@@ -175,6 +176,7 @@ users_router.get('/find_user/', async(req, res) => {
             message: "missing login",
             status: 400
         })
+        return;
     }
     let query = User_account.findOne({login: req.query.user});
     const result = await query.exec();
@@ -192,6 +194,77 @@ users_router.get('/find_user/', async(req, res) => {
         status: 404
     });
 })
+
+users_router.get('/get_plans', async (req, res) => {
+    if(!req.query.api_key) {
+        res.status(403).json({
+            title: "Access denied",
+            message: "API key not provided!",
+            status: 403
+        })
+        return;
+    }
+    if(!await check_key(req.query.api_key)) {
+        res.status(403).json({
+            title: "Access denied",
+            message: "API key invalid!",
+            status: 403
+        })
+        return;
+    }
+    if(req.query.user_id == null) {
+        res.status(400).json({
+            title: "Bad Request",
+            message: "missing id!",
+            status: 400
+        })
+    }
+
+    let query = User_account.findOne({id: req.query.user_id});
+    const result = await query.exec();
+    if(result === null) {
+
+        res.status(404).json({
+            title: "Requested resource not found!",
+            message: "A user with this login was not found. Are you sure the login is correct?",
+            status: 404
+        })
+        return;
+    }
+    // res.status(200).json({
+    //     title: "user found!",
+    //     message: result.id,
+    //     status: 404
+    // });
+
+    let user_plans = Array.from(result.training_plans);
+    let plan_query = Plan.find({
+        id: { $in: user_plans
+            }  })
+
+    // let plan_query = Plan.findOne({
+    //          'id': user_plans[0]
+    // })
+    const plans_result = await plan_query.exec();
+
+
+    if(plans_result === null) {
+        res.status(404).json({
+            title: "Requested resource not found!",
+            message: "A plan with this id was not found. Are you sure the id is correct?",
+            status: 404
+        })
+        return;
+    }
+
+    res.status(200).json({
+        title: "user's plans found!",
+        message: plans_result,
+        status: 200
+    });
+
+
+});
 
 users_router.post('/add_plan_user_id', async (req, res) => {
     if(!req.query.api_key) {
@@ -221,7 +294,8 @@ users_router.post('/add_plan_user_id', async (req, res) => {
         req.body.sport == null ||
         req.body.duration_in_minutes == null ||
         req.body.priority == null ||
-        req.body.location == null)
+        req.body.location == null ||
+        req.body.weekday == null)
     {
         res.status(400).json({
             title: "Bad Request",
@@ -245,18 +319,22 @@ users_router.post('/add_plan_user_id', async (req, res) => {
         return;
     }
     let new_plan = new plan({
+        id: plan_id,
         name: req.body.name,
         date_created: get_date(),
         sport: sport_id,
         duration_in_minutes: req.body.duration_in_minutes,
         priority: req.body.priority,
-        location: req.body.location
+        location: req.body.location,
+        weekday: req.body.weekday
     })
+
 
 
     const query = User_account.findOneAndUpdate({id: req.query.id}, { $push: {training_plans: {
                 _id: plan_id
             }}});
+
     let result = await query.exec();
     const saved_plan = new_plan.save();
     if(result === null || saved_plan === null) {
--- api/schemas/plan.js
+++ api/schemas/plan.js
@@ -7,6 +7,7 @@ export const planSchema = new mongoose.Schema({
     sport: {type: mongoose.Schema.Types.ObjectId},
     duration_in_minutes: {type: Number},
     priority: {type: String, enum: ['high', 'medium', 'low']},
+    weekday: {type: Number, enum:[1,2,3,4,5,6,7]},
     location: {
         lat: {type: Number},
         lng: {type: Number}
